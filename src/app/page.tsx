505f42fe5ebdc56f0dc25f66de9feadfb93a6910
Adnan <adbak.dev@gmail.com>
2023-08-02 22:51:34 +0200

    home

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,7 +5,10 @@ const Home = () => {
     <div className="w-full h-full flex justify-between bg-black relative">
       <div className="max-w-screen-xl w-full h-full flex relative">
         <LeftSidebar />
-        <main className="ml-[275px]">Home</main>
+        <main className="ml-[275px] p-6 flex w-[600px] h-full min-h-screen flex-col border-l-[0.5px] border-r-[0.5px] border-gray-600">
+          <h1 className="text-xl font-bold">Home</h1>
+          <div className="border-t-[0.5px] border-r-[0.5px] border-gray-600"></div>
+        </main>
         <section></section>
       </div>
     </div>
